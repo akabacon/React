29431c5b94e5e4d1ffa646c96331c130d153b1c9
akabacon <next30jeeme@outlook.com>
2023-05-03 21:35:41 +0800

    Add files via upload

--- code_box/CodeEditor.js
+++ code_box/CodeEditor.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
-const CodeEditor=()=> {
-  const [code, setCode] = useState('');
 
-  const handleChange = (editor, data, value) => {
+const CodeEditor=()=> {
+  const [code, setCode] = useState(''); //code ,string format
+  const handleChange = (editor, data, value) => { //
     setCode(value);
   };
 
   return (
     <CodeMirror
-      value={code}
-      onBeforeChange={handleChange}
+      value={code} //words in box
+      onBeforeChange={handleChange} //handle  number
       options={{
         mode: 'javascript',
         theme: 'material',
